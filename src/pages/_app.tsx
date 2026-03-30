038a94c29204f876c2b77260f03b4b1bb78d3894
aryanbatras <batraaryan03@gmail.com>
2026-03-30 18:46:38 +0530

    auth-frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import { ClerkProvider, SignInButton, SignUpButton, Show, UserButton } from '@clerk/nextjs'
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider
+      {...pageProps}
+      appearance={{
+        cssLayerName: 'clerk',
+      }}
+    >
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
 }
+
+export default MyApp
